e079636d328fae3d704e4ceadde479df6d5057fc
IsmatKabir <Auni858@gmail.com>
2022-04-10 17:54:17 -0400

    first commit

---
+++ src/demoE/DemoE1.java
@@ -0,0 +1,11 @@
+package demoE;
+
+public class DemoE1 {
+    public static void main(String[] args) {
+		System.out.println("this is java script");
+		
+		
+		
+		System.out.println("aumi");
+	}
+}
