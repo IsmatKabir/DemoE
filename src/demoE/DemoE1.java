c985c4131996f00e6ae4526cce4dc84a3a8046c4
IsmatKabir <Auni858@gmail.com>
2022-04-10 20:59:59 -0400

    first commit

--- src/demoE/DemoE1.java
+++ src/demoE/DemoE1.java
@@ -4,7 +4,7 @@ public class DemoE1 {
     public static void main(String[] args) {
 		System.out.println("this is java script");
 		
-		
+		System.out.println(" we are practicing git hub");
 		
 		System.out.println("aumi");
 	}
